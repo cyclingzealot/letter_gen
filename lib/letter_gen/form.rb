0105925b71e20f107cd4a85c0595b8e9baae46e4
Ondřej Červenka <cerveon3@fit.cvut.cz>
2017-02-02 09:19:00 +0100

    formating

--- lib/letter_gen/form.rb
+++ lib/letter_gen/form.rb
@@ -2,7 +2,6 @@ require 'Qt'
 require 'fileutils'
 require 'json'
 
-
 require_relative 'form_field'
 
 # Basic Form class
@@ -69,7 +68,6 @@ end
 # Company's secretary information
 class FormSecretary < Form
   def initialize
-
     form_fields = {
       secretary_name: TextField.new('Jméno jednatele'),
       secretary_street: TextField.new('Ulice'),
@@ -84,7 +82,6 @@ end
 # Company information
 class FormCompany < Form
   def initialize
-
     form_fields = {
       company_name: TextField.new('Název společnosti'),
       company_street: TextField.new('Sídlo společnosti - ulice'),
@@ -100,7 +97,6 @@ end
 # Finance bureau information
 class FormGov < Form
   def initialize
-
     form_fields = {
       local_office: TextField.new('Územní pracoviště'),
       finance_address: TextField.new('Sídlo FÚ - ulice'),
@@ -114,13 +110,11 @@ end
 
 # User's profile
 class FormUser < Form
-
   PROFILE_PATH = "#{Dir.home}/.config/letter_gen/profiles".freeze
 
   slots 'save_profile()', 'load_profile()'
 
   def initialize
-
     form_fields = {
       user_name: TextField.new('Jméno'),
       user_address: TextField.new('Adresa - ulice'),
@@ -140,31 +134,26 @@ class FormUser < Form
     loadb.setFixedSize(BUTTON_X_SIZE, BUTTON_Y_SIZE)
     connect(loadb, SIGNAL('clicked()'), SLOT('load_profile()'))
 
-
     @layout.addWidget saveb
-    @layout.addWidget loadb 
+    @layout.addWidget loadb
   end
 
   def save_profile
     FileUtils.mkdir_p(PROFILE_PATH)
-    File.open("#{PROFILE_PATH}/profile.json", 'w') { |f| f.write(self.to_dict.to_json) }
-    @status_bar.text = 'Uloženo'    
+    File.open("#{PROFILE_PATH}/profile.json", 'w') { |f| f.write(to_dict.to_json) }
+    @status_bar.text = 'Uloženo'
   end
 
   def load_profile
-    
-    unless File.exists?("#{PROFILE_PATH}/profile.json")
+    unless File.exist?("#{PROFILE_PATH}/profile.json")
       @status_bar.text = 'Nenalezen žádný profil'
       return
     end
-      
     data = nil
-    File.open("#{PROFILE_PATH}/profile.json", 'r') { |f| data = JSON.parse(f.read())}
+    File.open("#{PROFILE_PATH}/profile.json", 'r') { |f| data = JSON.parse(f.read) }
     data.each do |key, value|
       @form_fields[key.to_sym].text_field.text = value
     end
     @status_bar.text = 'Načteno'
   end
 end
-
-
